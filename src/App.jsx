d04c5af32d415405c765c4bda9dc4a632f482954
Daniel Echeverri <echeverri121@gmail.com>
2023-12-06 19:50:44 -0500

    Router added but not available to merge it with Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
-import { useState } from 'react'
-import './App.css'
-import { BrowserRouter, Router } from 'react-router-dom'
-import NavBar from './components/NavBar/NavBar'
+import { useState } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar/NavBar';
+import Contact from './components/Contact/Contact';
+import Home from './components/Home/Home';
+import Projects from './components/Projects/Projects';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <BrowserRouter>
-        <NavBar/>
-      </BrowserRouter>
+    <NavBar />
+    <Router>
+      <>        
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </>
+    </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,14 @@
+import { Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function NavBar() {
+const NavBar = () => {
   return (
-    <>
-      <Navbar bg="dark" data-bs-theme="dark">
+    <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">DELL</Navbar.Brand>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#features">Features</Nav.Link>
@@ -16,8 +16,7 @@ function NavBar() {
           </Nav>
         </Container>
       </Navbar>
-    </>
   );
-}
+};
 
 export default NavBar;
