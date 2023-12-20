b5c10929beef318a09ad875f332bec6864523cc5
Daniel Echeverri <echeverri121@gmail.com>
2023-12-20 15:13:38 -0500

    Correct styling in presentation, add routing to contact and increase size of form labels

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,6 +11,9 @@ function Home() {
     function handleClick() {
       navigate('/projects');
     }
+    function handleClickC() {
+        navigate('/contact');
+      }
     return ( 
         <>
             <main className={styles.main}>
@@ -33,7 +36,7 @@ function Home() {
                 <section className={styles.sectionA}>
                     <h1><u>About me</u></h1>
                     
-<h3>I am a former teacher with a passion for software development. I hold a Front-end Engineering certification from Codecademy, establishing a robust foundation in web development technologies. Additionally, I am certified in backend technologies and proficient in various programming languages, including Python and C#. I consider myself a dedicated individual who enjoys learning new things, excels as a team player, and demonstrates strong organizational skills and responsibility. My current objective is to secure a challenging and dynamic position where I can share my knowledge while continually acquiring new experiences and skills. On this portfolio website, you can find an overview of all the <u className={styles.u} onClick={handleClick}>projects</u> I have successfully completed.</h3>
+<h3>I am a former teacher with a passion for software development. I hold a Front-end Engineering certification from Codecademy, establishing a robust foundation in web development technologies. Additionally, I am certified in backend technologies and proficient in various programming languages, including Python and C#. I consider myself a dedicated individual who enjoys learning new things, excels as a team player, and demonstrates strong organizational skills and responsibility. My current objective is to secure a challenging and dynamic position where I can share my knowledge while continually acquiring new experiences and skills. On this portfolio website, you can find an overview of all the <u className={styles.u} onClick={handleClick}>projects</u> I have successfully completed. If you need to contact me, you can write to me <u className={styles.u} onClick={handleClickC}>here</u> or at my email address <i className="fa-solid fa-arrow-down-long"></i>.</h3>
                 </section>
             </article>
             <Skills/>
